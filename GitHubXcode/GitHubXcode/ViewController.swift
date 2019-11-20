fcfaf8b9cdf3d8c64d2ea2fe69a362b181218ecd
Amit <ashrivastva@crestron.com>
2019-11-20 10:08:55 -0600

    My First Commit

---
+++ GitHubXcode/GitHubXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubXcode
+//
+//  Created by Amit Shrivastva on 11/20/19.
+//  Copyright © 2019 Amit Shrivastva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
